732908173a760af7dce63fde7471cc664de500b8
Jesse Sandvik <sandvikjesse@gmail.com>
2025-02-23 18:04:47 -0500

    added tests for plugin manager

---
+++ tests/application/plugin/test_plugin_manager.py
@@ -0,0 +1,44 @@
+import unittest
+
+from unittest.mock import MagicMock
+
+from src.domain import Plugin, IPluginLoader, IPluginRegistry, IPluginValidator
+from src.application import PluginManager
+
+
+class MockPlugin:
+    def __init__(self, name):
+        self.instance = type(name, (), {})()
+
+
+class TestPluginManager(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_loader = MagicMock(spec=IPluginLoader)
+        self.mock_registry = MagicMock(spec=IPluginRegistry)
+        self.mock_validator = MagicMock(spec=IPluginValidator)
+        self.plugin_manager = PluginManager(
+            self.mock_loader, self.mock_registry, self.mock_validator
+        )
+
+    def test_load_plugins_should_call_plugin_loader_load_plugins(self):
+        self.plugin_manager.load_plugins()
+        self.mock_loader.load_plugins.assert_called_once()
+
+    def test_register_plugins_should_call_validator_validate(self):
+        mock_plugins = [
+            Plugin(None, instance=MockPlugin("PluginA"))
+        ]
+        self.mock_loader.plugins = mock_plugins
+    
+        self.plugin_manager.register_plugins()
+        self.mock_validator.validate.assert_called_once_with(mock_plugins[0])
+
+    def test_register_plugins_should_call_registry_register_plugin(self):
+        mock_plugins = [
+            Plugin(None, instance=MockPlugin("PluginA"))
+        ]
+        self.mock_loader.plugins = mock_plugins
+    
+        self.plugin_manager.register_plugins()
+        self.mock_registry.register_plugin.assert_called_once_with(mock_plugins[0])
