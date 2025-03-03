4a03714770be8f70c82941237027e749e8f0e4a7
Jesse Sandvik <sandvikjesse@gmail.com>
2025-03-03 06:01:30 -0500

    established abstract class for plugin metadata repository

--- src/virtual_assistant/domain/plugin/metadata/plugin_metadata_repository.py
+++ src/virtual_assistant/domain/plugin/metadata/plugin_metadata_repository.py
@@ -1,28 +1,27 @@
 from abc import ABC, abstractmethod
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 
 from .plugin_metadata import PluginMetadata
 
 
 class PluginMetadataRepository(ABC):
-    __plugin_metadata: Dict[str, PluginMetadata]
 
     @abstractmethod
-    def save(self, plugin_metadata: PluginMetadata) -> None:
-        self.__plugin_metadata[plugin_metadata.entry_point] = plugin_metadata
+    def create_plugin_metadata(self, plugin_metadata: PluginMetadata) -> Optional[PluginMetadata]:
+        pass
     
     @abstractmethod
     def get_plugin_metadata_by_entry_point(self, entry_point: str) -> Optional[PluginMetadata]:
-        return self.__plugin_metadata.get(entry_point, None)
+        pass
     
     @abstractmethod
-    def get_all_plugin_metadata(self) -> Dict[str, PluginMetadata]:
-        return self.__plugin_metadata.items()
+    def get_all_plugin_metadata(self) -> Optional[List[Dict[str, PluginMetadata]]]:
+        pass
+
+    @abstractmethod
+    def update_plugin_metadata_by_entry_point(self, entry_point: str) -> Optional[PluginMetadata]:
+        pass
     
     @abstractmethod
-    def remove_plugin_metadata_by_entry_point(self, entry_point: str) -> None:
-        self.__plugin_metadata.pop(entry_point, None)
-    
-    @abstractmethod
-    def exists(self, entry_point: str) -> bool:
-        return entry_point in self.__plugin_metadata.keys()
+    def remove_plugin_metadata_by_entry_point(self, entry_point: str) -> bool:
+        pass
